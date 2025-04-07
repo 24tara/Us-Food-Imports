77da87e7d78644bf65633fe9e7156916b7198ae0
T <163727484+24tara@users.noreply.github.com>
2025-04-07 19:49:09 +0000

    #here i have uploaded the dataset that i am going to use for my app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Analytics of US Food Imports")
+
+import pandas as pd
+#uploading and reading the cleaned dataset
+dataset = pd.read_csv('FoodImports.csv')
+
